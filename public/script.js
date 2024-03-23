13cdf7b187b4406b33012a269dfb2d0cfdb7f05f
aarindey <aarindey069@gmail.com>
2024-03-24 04:01:33 +0530

    code added

---
+++ public/script.js
@@ -0,0 +1,27 @@
+const userVideo = document.getElementById("user-video");
+const startButton = document.getElementById("start-btn");
+
+const state = { media: null };
+const socket = io();
+
+startButton.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 2500000,
+    framerate: 25,
+  });
+  mediaRecorder.ondataavailable = (ev) => {
+    console.log("binary encoded video stream data ", ev.data);
+    socket.emit("binarystream", ev.data);
+  };
+  mediaRecorder.start(25);
+});
+
+window.addEventListener("load", async (e) => {
+  const media = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  userVideo.srcObject = media;
+  state.media = media;
+});
